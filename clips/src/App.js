8e311aefd5f36bb654b85fb5619ba90e76cd1ec6
ncy <nongchangyin@xd.com>
2023-05-06 20:50:40 +0800

    -

--- clips/src/App.js
+++ clips/src/App.js
@@ -1,5 +1,8 @@
-import logo from "./logo.svg";
+import logo from "./clips-logo.svg";
 import "./App.css";
+import {useState, useEffect} from 'react';
+import abi from './abi.json'
+
 
 import "@rainbow-me/rainbowkit/styles.css";
 import {
@@ -8,11 +11,13 @@ import {
     midnightTheme,
     darkTheme,
 } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+
+import { configureChains, createClient, WagmiConfig , useSigner, useProvider} from "wagmi";
+import { mainnet, polygon, optimism, arbitrum ,goerli, sepolia} from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {Contract, utils} from "ethers";
 
 const { chains, provider } = configureChains([arbitrum], [publicProvider()]);
 
@@ -28,32 +33,98 @@ const wagmiClient = createClient({
     provider,
 });
 
-function App() {
+let contractAddr = '0xB9D2AecDbF874cC29Bb5C8785dA188eB704222B8'
+
+
+function Main() {
+    let [clipsBalance, setClipsBalance] = useState(0)
+    let [prizePool, setPrizePool] = useState(0)
+    let [clipOwner, setClipOwner] = useState('')
+    let [clip, setClip] = useState('')
+    let [clipLink, setClipLink] = useState('')
+    let [timeLeft, setTimeLeft] = useState(0)
+    let [canMint, setCanMint] = useState(true)
+    let [mintAmout, setMintAmount] = useState(0)
+
+    let {data} = useSigner()
+    let provider = useProvider()
+    let signer = data
+    let clips = new Contract(contractAddr, abi, signer) 
+    let clipsPub = new Contract(contractAddr, abi, provider)
+    function formatTime(seconds) {
+        const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+        const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+        const remainingSeconds = String(Math.floor(seconds % 60)).padStart(2, '0');
+        return `${hours}:${minutes}:${remainingSeconds}`;
+    }
+
+    async function mint() {
+        let liq = await clips.liquidity()
+        await clips.mintClips({value : liq})
+    }
+
+    useEffect(() => {
+        let interval = setInterval(async ()=>{
+            setPrizePool(utils.formatEther(await clipsPub.prizePool()))
+            setClip(await clipsPub.clip())
+            setClipLink(await clipsPub.clipLink())
+            setClipOwner(await clipsPub.clipOwner())
+            setMintAmount(utils.formatEther(await clipsPub.initialMintAmount())) 
+            let last = Number(await clipsPub.lastClipUpdate())
+            if (last > 0) {
+                let now = Math.floor(Date.now() / 1000)
+                setTimeLeft(last+ 7*24*60*60 - now)
+            }
+
+            if (!signer) {
+                return
+            }
+            let address = await signer.getAddress()
+            let balance = await clips.balanceOf(address)
+            setClipsBalance(utils.formatEther(balance))
+            
+        }, 1000)
+        return () => clearInterval(interval);
+    })
+    return (
+        <div className="App">
+        <nav className="w-full flex fixed top-0 left-0 px-8 py-4 justify-between items-center space-x-6">
+            <div>
+                <img width="32" height="32" src={logo}/> 
+            </div>
+            <div className="items-center space-x-8 hidden md:flex">
+                <div> {clipsBalance} $clips</div>
+                <ConnectButton label="connect" />
+            </div>
+        </nav>
+        <div className="min-h-screen flex justify-center items-center pt-16 md:pt-0">
+            <div className="h-min flex flex-col">
+                <p className="text-lg md:text-2xl text-white font-black text-center"> prizepool: {prizePool} $clips</p>
+                <p className="text-white font-black text-center text-lg md:text-2xl"> CLIP OWNER 👇: {clipOwner}</p>
+                <a className="" href={clipLink}>
+
+                    <h2 className="Clip text-transparent bg-gradient-to-r from-[#00ffa0] via-[#69b6ff] to-[#cd14ff] bg-clip-text font-black text-6xl md:text-6xl lg:text-9xl text-center my-12 uppercase break-words max-w-[90vw] md:max-w-[90vw]">
+                        {clip}
+                    </h2>
+                </a>
+                <div className="text-white text-center text-6xl font-black mb-4">
+                    <p className="text-center mb-2 ">OWNER CAN CLAIM PRIZEPOOL IN:</p>
+                    {formatTime(timeLeft)}
+                </div>
+                <button onClick={mint} className="btn-gradient mt-4 self-center w-full md:w-auto " disabled={!canMint}>Mint {mintAmout} $clips</button>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+
+
+function App() { 
     return (
         <WagmiConfig client={wagmiClient}>
-            <RainbowKitProvider chains={chains} theme={darkTheme()}>
-                <div className="App">
-                    <header className="fixed top-0 w-full p-4">
-                        <nav className="w-full flex fixed top-0 left-0 px-8 py-4 justify-between items-center space-x-6">
-                            <div>logo</div>
-                            <div className="flex flex-row gap-2">
-                                <div>$clips</div>
-                                <ConnectButton />
-                            </div>
-                        </nav>
-                    </header>
-                    <div className="min-h-screen flex justify-center items-center pt-16 md:pt-0">
-                        <div className="h-min flex flex-col">
-        <div className=""> prizepool: 20000000 $clips</div>
-        <div className=""> clip owner: 0x00000000000000000000</div>
-                            <h2 className="Clip leading-tight m-0 text-transparent bg-gradient-to-r from-[#00ffa0] via-[#69b6ff] to-[#cd14ff] bg-clip-text font-black text-3xl md:text-3xl lg:text-7xl text-center my-12 uppercase break-words max-w-[90vw] md:max-w-[90vw]">
-        abcdefghijklmnopqrstuvwxyz
-                            </h2>
-                            <div className="">owner can claim prizepool in</div>
-                            <button className="btn-gradient mt-4 self-center w-full md:w-auto">Mint 5000000 $clips</button>
-                        </div>
-                    </div>
-                </div>
+            <RainbowKitProvider chains={chains} theme={ darkTheme({ accentColor: '#5b3fe4', accentColorForeground: 'white', fontStack: 'system', overlayBlur: 'small', })}>
+                    <Main/>
             </RainbowKitProvider>
         </WagmiConfig>
     );
