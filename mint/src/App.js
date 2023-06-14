b2c56378c69bc7b283307b99e9ae4349a889ee40
ncy <nongchangyin@xd.com>
2023-06-14 16:04:58 +0800

    -

--- mint/src/App.js
+++ mint/src/App.js
@@ -29,7 +29,7 @@ import { goerli, zkSync, zkSyncTestnet } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Contract, utils } from "ethers";
+import { Contract, utils , BigNumber} from "ethers";
 const {abi} = require('./abi.json');
 
 SwiperCore.use([Autoplay, Navigation]);
@@ -681,7 +681,7 @@ function Main() {
     let [panelOp, setPanelOp] = useState(0)
     let [minted, setMinted] = useState(0)
     let [total, setTotal] = useState(0)
-    let [mintPrice, setMintPrice] = useState(0)
+    let [mintPrice, setMintPrice] = useState(BigNumber.from(0))
     let [minting, setMinting] = useState(false)
     let [isWhitelisted, setIsWhitelisted] = useState(false)
     let [proof, setProof] = useState([])
@@ -694,6 +694,7 @@ function Main() {
     let [msgCls, setMsgCls] = useState('opacity-0')
     let [isPhone, setIsPhone] = useState(false)
     let [phonectl, setPhoneCtl] = useState(false)
+    let [updateFee, setUpdateFee] = useState(false)
 
     let {data:signer} = useSigner()
     let provider = useProvider()
@@ -732,32 +733,37 @@ function Main() {
         let end = 1
         return p * ((end-start)*2/t) + start
     }
+
+    let upFee = async() => {
+        try {
+            let {fee, isWl} = await signerContract.mintFee(mintAmount, proof)
+            if (isWl && fee == mintPrice*mintAmount) {
+                setWlClaimed(true)
+            } else {
+                setWlClaimed(false)
+            }
+            setMintFee(fee)
+        }catch(e) {
+        }
+    }
+
     let updateMintMenu = async () => {
         if (signer) {
-                try {
-                    let {data} = await axios.get(`proof/${await signer.getAddress()}`)
-                    setProof(data)
-                    if (data.length> 0) {
-                        setIsWhitelisted(true)
-                    }else {
-                        setIsWhitelisted(false)
+            try {
+                let {data} = await axios.get(`proof/${await signer.getAddress()}`)
+                setProof(data)
+                if (data.length> 0) {
+                    setIsWhitelisted(true)
+                }else {
+                    setIsWhitelisted(false)
                     }
 
                     try {
-                        //providerContract.mintPrice().then(setMintPrice)
+                        providerContract.mintPrice().then(setMintPrice)
                     }catch(e){
                     }
                     let so = updateMintProgress()
-                        try {
-                            let {fee, isWl} = await signerContract.mintFee(mintAmount, data)
-                            if (isWl && fee == mintPrice*mintAmount) {
-                                setWlClaimed(true)
-                            } else {
-                                setWlClaimed(false)
-                            }
-                            setMintFee(fee)
-                        }catch(e) {
-                        }
+                    
                 }catch(e) {
                     showMsg(e.message)
                 }
@@ -797,6 +803,7 @@ function Main() {
         }
     }, [slogenGray])
 
+    upFee()
     async function onMint() {
         showMsg('Mint start...', sleep(3000))
         if (minting) {
@@ -854,8 +861,8 @@ function Main() {
                         <div className="norse pt-6 lg:p-6 w-full flex flex-row justify-end ">
                             <div style={{ opacity: panelOp }} className="flex flex-col md:flex-row gap-1 md:gap-4 ">
                                 {isWhitelisted &&
-                                <div className="h-fit btn text-accent broder-accent">
-                                    whitelist {wlClaimed && "claimed"}
+                                        <div className="h-fit btn text-accent broder-accent">
+                                        whitelist {wlClaimed && "claimed"}
                                 </div>
                                 }
                                 <ZERC20ConnectButton />
@@ -924,11 +931,20 @@ function Main() {
                                         <div className="row items-center gap-4 w-full">
                                             <MintAmount selectedIdx={mintAmount-1} start={1} end={5} onSelected={ async amount => {
                                                 setMintAmount(amount) 
-                                                setTimeout(updateMintMenu, 100)
-                                            }}/>
+                                                setUpdateFee(true)
+                                                await sleep(100)
+                                                await upFee()
+                                                setUpdateFee(false)
+                                                }}/>
                                         </div>
-                                        <div className="md:text-xl w-full text-right">
-                                            total: {utils.formatEther(mintFee)}{isWhitelisted && !wlClaimed &&  <span className="text-green-500">(${utils.formatEther(mintPrice)* (mintAmount)}-${utils.formatEther(mintPrice)})</span>}Ξ
+                                        <div className="md:text-xl w-full text-right row items-center justify-end">
+                                            {updateFee &&
+                                                    <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                                                    </svg>
+                                            }
+                                            total: {utils.formatEther(mintFee)}{isWhitelisted && !wlClaimed &&  <span className="text-green-500">(${utils.formatEther(mintPrice.mul(mintAmount))}-${utils.formatEther(mintPrice)})</span>}Ξ
                                         </div>
                                     </div>
                                     <div className="w-full flex flex-col md:flex-row  gap-2 px-2">
